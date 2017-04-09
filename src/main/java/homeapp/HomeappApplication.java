2efd07e630f6153a86e1511a5042d50023562a1f
t-matsumo <tatsukimatsumo@gmail.com>
2017-04-09 09:51:51 +0000

    Add skeltons

---
+++ src/main/java/homeapp/HomeappApplication.java
@@ -0,0 +1,12 @@
+package homeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeappApplication.class, args);
+	}
+}
