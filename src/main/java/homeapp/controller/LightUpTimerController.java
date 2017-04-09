7aca557581a8e7613deb9722ea42511f130275b9
t-matsumo <tatsukimatsumo@gmail.com>
2017-04-09 12:38:48 +0000

    Before implement

--- src/main/java/homeapp/controller/LightUpTimerController.java
+++ src/main/java/homeapp/controller/LightUpTimerController.java
@@ -13,33 +13,33 @@ public class LightUpTimerController {
     return "lightUpTimer/index";
   }
 
-  @RequestMapping(value = "/new", method = RequestMethod.GET)
-  public String createForm() {
-    return "lightUpTimer/createForm";
-  }
+  // @RequestMapping(value = "/new", method = RequestMethod.GET)
+  // public String createForm() {
+  //   return "lightUpTimer/createForm";
+  // }
 
-  @RequestMapping(value = "/new", method = RequestMethod.POST)
-  public String create() {
-    return "redirect: /lightUpTimers";
-  }
+  // @RequestMapping(value = "/new", method = RequestMethod.POST)
+  // public String create() {
+  //   return "redirect: /lightUpTimers";
+  // }
 
-  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  public String detail(@PathVariable("id") int id) {
-    return "lightUpTimer/detail";
-  }
+  // @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  // public String detail(@PathVariable("id") int id) {
+  //   return "lightUpTimer/detail";
+  // }
 
-  @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
-  public String editForm(@PathVariable("id") int id) {
-    return "lightUpTimer/editForm";
-  }
+  // @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
+  // public String editForm(@PathVariable("id") int id) {
+  //   return "lightUpTimer/editForm";
+  // }
 
-  @RequestMapping(value = "/{id}/edit", method = RequestMethod.POST)
-  public String edit(@PathVariable("id") int id) {
-    return "redirect: /lightUpTimers/" + id;
-  }
+  // @RequestMapping(value = "/{id}/edit", method = RequestMethod.POST)
+  // public String edit(@PathVariable("id") int id) {
+  //   return "redirect: /lightUpTimers/" + id;
+  // }
 
-  @RequestMapping(value = "/{id}/delete", method = RequestMethod.POST)
-  public String delete(@PathVariable("id") int id) {
-    return "redirect: /lightUpTimers";
-  }
+  // @RequestMapping(value = "/{id}/delete", method = RequestMethod.POST)
+  // public String delete(@PathVariable("id") int id) {
+  //   return "redirect: /lightUpTimers";
+  // }
 }
